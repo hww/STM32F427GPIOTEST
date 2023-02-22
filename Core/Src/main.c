429f0a13054005168d171d50dc240b59424ad760
hww <valery.hww@gmail.com>
2023-02-22 23:28:22 +0300

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -254,20 +254,25 @@ void StartDefaultTask(void *argument)
   for(;;)
   {
 	/* A BABY TALK */
+
+	// Read the inputs
 	GPIO_PinState gf6 = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_6);
 	GPIO_PinState gf7 = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_7);
 	GPIO_PinState gf8 = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_8);
 	GPIO_PinState gf9 = HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9);
 
+	// GPIO_PinState has 0 or 1 integer value, so then can be used bitwise and
+	// it is less portable but good enough for test
 	int gf10 = (int)gf6 & (int)gf7;
 	int gf11 = (int)gf8 & (int)gf9;
 
+	// Write the outputs
 	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_0, gf6);
 	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_1, gf7);
 	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_2, gf8);
 	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_3, gf9);
-	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, gf10 ? GPIO_PIN_SET : GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_11, gf11 ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, gf10);
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_11, gf11);
 
     osDelay(10);
   }
